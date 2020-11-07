b2a5a37f67a617eed585a94591982db9241de051
AnnaN <43808024+AnnaNCS@users.noreply.github.com>
2020-11-07 11:17:03 -0800

    Add files via upload
    starter

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php include "header.php"; ?>
+
+    <ul>
+      <li>
+        <a href="create.php"><strong>Create</strong></a> - add a user
+      </li>
+      <li>
+        <a href="read.php"><strong>Read</strong></a> - find a user
+      </li>
+    </ul>
+  </body>
+</html>
+
+<?php include "footer.php"; ?>
